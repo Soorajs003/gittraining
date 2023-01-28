69e3cbb5ce27693819ede28d673cd6a980933996
umeshuday <umeshuday788@gmail.com>
2023-01-28 15:24:10 +0530

    intial umesh commit

--- file.js
+++ file.js
@@ -1,3 +1,17 @@
 const commmand ="1"
 
 const bla = " blaa blaa"
+
+export const usePagination = ({
+    totalCount,
+    pageSize,
+    siblingCount = 1,
+    currentPage
+  }) => {
+    const paginationRange = useMemo(() => {
+       // Our implementation logic will go here 
+        
+    }, [totalCount, pageSize, siblingCount, currentPage]);
+  
+    return paginationRange;
+  };
