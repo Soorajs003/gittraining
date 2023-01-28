2632b651e37a20ac2d6dfbc6b4026b9d467ca3d2
umeshuday <umeshuday788@gmail.com>
2023-01-28 15:55:59 +0530

    type1

--- hello/New Text Document.js
+++ hello/New Text Document.js
@@ -1,2 +1,14 @@
 const hello = "hello"
 const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"const hello = "hello"
+xport const usePagination = ({
+    totalCount,
+    pageSize,
+    siblingCount = 1,
+    currentPage
+  }) => {
+    const paginationRange = useMemo(() => {
+       // Our implementation logic will go here 
+        
+    }, [totalCount, pageSize, siblingCount, currentPage]);
+  
+    return pagin
